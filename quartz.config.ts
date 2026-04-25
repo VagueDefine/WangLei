5475a2e79fac7e80b2c244ecae5ad31c77766861
vague <vague@vague.com>
2026-04-25 16:25:43 +0800

    配置 GitHub Pages 部署

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "vaguedefine.github.io/WangLei.github.io/",
+    baseUrl: "vaguedefine.github.io/WangLei/",
     ignorePatterns: [
       "private", 
       "**/000templates", 

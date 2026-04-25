bd43819a77ef3a750b30a7a0bccbdcc21bba877e
vague <vague@vague.com>
2026-04-25 21:46:28 +0800

    修改

--- content/cleanup-git.ps1
+++ content/cleanup-git.ps1
@@ -1,6 +1,14 @@
 # Delete .git folders in current folder and all subfolders
 Get-ChildItem -Path . -Recurse -Directory -Filter ".git" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
+Get-ChildItem -Path . -Recurse -Directory -Filter ".obsidian" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+
+Get-ChildItem -Path . -Recurse -Directory -Filter ".trash" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+
+Get-ChildItem -Path . -Recurse -Directory -Filter "000Excaildraw" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+
+Get-ChildItem -Path . -Recurse -Directory -Filter "00templates" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+
 # Also delete .gitignore and .gitattributes files
 Get-ChildItem -Path . -Recurse -File -Filter ".gitignore" -Force | Remove-Item -Force -ErrorAction SilentlyContinue
 Get-ChildItem -Path . -Recurse -File -Filter ".gitattributes" -Force | Remove-Item -Force -ErrorAction SilentlyContinue

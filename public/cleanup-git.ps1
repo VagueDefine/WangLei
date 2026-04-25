a4ebd70a50f8392929897f6fd1b5827a03f424b5
vague <vague@vague.com>
2026-04-25 20:42:03 +0800

    docs: 删除重复文件

--- public/cleanup-git.ps1
+++ public/cleanup-git.ps1
@@ -1,5 +1,8 @@
-# Delete .git folders and files in subfolders
-Get-ChildItem -Path . -Recurse -Directory -Filter ".git" -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
-Get-ChildItem -Path . -Recurse -File -Filter ".gitignore" -ErrorAction SilentlyContinue | Remove-Item -Force -ErrorAction SilentlyContinue
-Get-ChildItem -Path . -Recurse -File -Filter ".gitattributes" -ErrorAction SilentlyContinue | Remove-Item -Force -ErrorAction SilentlyContinue
+# Delete .git folders in current folder and all subfolders
+Get-ChildItem -Path . -Recurse -Directory -Filter ".git" -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+
+# Also delete .gitignore and .gitattributes files
+Get-ChildItem -Path . -Recurse -File -Filter ".gitignore" -Force | Remove-Item -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path . -Recurse -File -Filter ".gitattributes" -Force | Remove-Item -Force -ErrorAction SilentlyContinue
+
 Write-Host "Done!"
